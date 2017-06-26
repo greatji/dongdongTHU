253844753e077a838e345447cd47942f38061171
dc3671 <chenzhh3671@gmail.com>
2017-06-27 00:06:46 +0800

    feat(new-ui): finish login, activity, club

--- static/js/index_control.js
+++ static/js/index_control.js
@@ -82,15 +82,3 @@ function setTab(name, cursel, n) {
         if (menu) menu.className = ((i == cursel) ? "hover" : "");
     }
 }
-
-var swiper = new Swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    slidesPerView: 1,
-    paginationClickable: true,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: 3000,
-    autoplayDisableOnInteraction: false
-});
